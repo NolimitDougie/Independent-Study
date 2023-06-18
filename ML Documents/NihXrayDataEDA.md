de490f6c8907f8f859850924760a3236eeb8b4dc
dougietownsell <townsell.4@wright.edu>
2023-06-17 22:59:36 -0400

    Updates to NIH code & EDA markdown file

---
+++ ML Documents/NihXrayDataEDA.md
@@ -0,0 +1,51 @@
+# NIH Chest Xray Dataset 
+
+
+
+
+
+
+## Data Distribution 
+
+```Finding Labels
+No Finding                  60361
+Infiltration                 9547
+Atelectasis                  4215
+Effusion                     3955
+Nodule                       2705
+Pneumothorax                 2194
+Mass                         2139
+Effusion|Infiltration        1603
+Atelectasis|Infiltration     1350
+Consolidation                1310
+Atelectasis|Effusion         1165
+Pleural_Thickening           1126
+Cardiomegaly                 1093
+Emphysema                     892
+Infiltration|Nodule           829
+```
+
+
+## Implementation 
+
+
+
+
+
+
+## Model 
+
+
+
+
+
+
+
+## Test Results 
+
+
+
+
+
+
+
--- NihXrayNN.py
+++ NihXrayNN.py
@@ -14,11 +14,9 @@ import seaborn as sns
 from tqdm import tqdm
 from PIL import Image
 import torch.nn.functional as F
-import cv2
-from sklearn.preprocessing import MultiLabelBinarizer
 import torch.optim as optim
 from torch.utils.data import TensorDataset
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 from sklearn.metrics import multilabel_confusion_matrix
 
 # Device configuration
@@ -41,11 +39,12 @@ all_image_paths = {os.path.basename(x): x for x in
 all_xray_df['path'] = all_xray_df['Image Index'].map(all_image_paths.get)
 all_xray_df.sample(3)
 
-# Disease Statistics
+# # Disease Statistics
 # num_unique_labels = all_xray_df['Finding Labels'].nunique()
 # print('Number of unique labels:', num_unique_labels)
 # count_per_unique_label = all_xray_df['Finding Labels'].value_counts()[:15]  # get frequency counts per label
 # print(count_per_unique_label)
+# exit()
 
 # Data Pre Processing ####
 # define condition labels for one hot encoding - simplifying to 14 primary classes (excl. No Finding)
@@ -62,9 +61,7 @@ all_xray_df.head()
 # eof of one hot encoding
 
 # Data Splitting ###
-train_df, test_df = train_test_split(all_xray_df, test_size=0.40, random_state=2020)
-
-
+train_df, test_df = train_test_split(all_xray_df, test_size=0.20, random_state=2020)
 #  eof Data Splitting ###
 
 
@@ -86,7 +83,7 @@ class XrayDataset(torch.utils.data.Dataset):
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
-        return transform(data), torch.FloatTensor(label)
+        return transform(data), torch.Tensor(label)
 
     def __len__(self):
         return len(self.data_frame)
@@ -105,7 +102,7 @@ test_loader = torch.utils.data.DataLoader(
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=10000,
+    batch_size=3000,
     num_workers=0,
     shuffle=True,
 )
@@ -129,33 +126,28 @@ torch.manual_seed(42)
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
+
         # Image size 256 * 256 * 3 input channels
         # 1. convolutional layer
         self.conv1 = nn.Conv2d(3, 32, 3)
         self.conv1_bn = nn.BatchNorm2d(32)
-        # outputs - (32 filter images, 254 * 254)
         # 2. convolutional layer
-        # sees 254 * 254 * 32 tensor (2x2 MaxPooling layer beforehand)
         self.conv2 = nn.Conv2d(32, 32, 3)
         self.conv2_bn = nn.BatchNorm2d(32)
-        # outputs 126 * 126 * 32 filtered images, kernel-size is 3
         # 3. convolutional layer
-        # sees 126 x 126 x 32 tensor (2x2 MaxPooling layer beforehand)
         self.conv3 = nn.Conv2d(32, 64, 3)
         self.conv3_bn = nn.BatchNorm2d(64)
-        # outputs 124 * 124 * 64 filtered images, kernel-size is 3
-        # 4 Convolution Layer
-        # 124 * 124 * 64 Image
-        self.conv4 = nn.Conv2d(64, 64, 3)
-        self.conv4_bn = nn.BatchNorm2d(64)
+        # 4 convolution Layer
+        self.conv4 = nn.Conv2d(64, 128, 3)
+        self.conv4_bn = nn.BatchNorm2d(128)
         # output tensor 61 * 61 * 64
         # 5. convolutional layer
-        self.conv5 = nn.Conv2d(64, 128, 3)
-        self.conv5_bn = nn.BatchNorm2d(128)
-        # outputs 59 * 59 * 128 filter images
-        # 6 convolutional layer
-        self.conv6 = nn.Conv2d(128, 128, 3)
-        self.conv6_bn = nn.BatchNorm2d(128)
+        # self.conv5 = nn.Conv2d(64, 128, 3)
+        # self.conv5_bn = nn.BatchNorm2d(128)
+        # # outputs 59 * 59 * 128 filter images
+        # # 6 convolutional layer
+        # self.conv6 = nn.Conv2d(128, 128, 3)
+        # self.conv6_bn = nn.BatchNorm2d(128)
         # outputs 28 * 28 * 128 filtered Images
         # self.conv7 = nn.Conv2d(128, 128, 3)
         # self.conv7_bn = nn.BatchNorm2d(128)
@@ -165,57 +157,43 @@ class ConvNet(nn.Module):
 
         # 1. fully-connected layer
         # Input is a flattened 28*28*128 dimensional vector
-        self.fc1 = nn.Linear(128 * 28 * 28, 128)
+        self.fc1 = nn.Linear(128 * 61 * 61, 128)
         self.fc1_bn = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 14)
-        # self.fc3 = nn.Linear(14, 10)
+        self.fc2 = nn.Linear(128, 100)
+        self.fc3 = nn.Linear(100, 14)
+        # self.output = nn.TransformerDecoder()
+        # self.sigmoid = nn.Sigmoid()
 
         # definition of dropout (dropout probability 25%)
         self.dropout20 = nn.Dropout(0.2)
         self.dropout30 = nn.Dropout(0.3)
-        self.dropout40 = nn.Dropout(0.4)
-        self.dropout50 = nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.conv1_bn(F.relu(self.conv1(x)))
-        # print(x.shape, "-- after 1st convolution layer --")
         x = self.pool(self.conv2_bn(F.relu(self.conv2(x))))
-        # print(x.shape, " --- after 2nd convolution layer --")
         x = self.dropout20(x)
         x = self.conv3_bn(F.relu(self.conv3(x)))
-        # print(x.shape, " -- after 3rd convolution layer --")
         x = self.pool(self.conv4_bn(F.relu(self.conv4(x))))
-        # print(x.shape, "-- after 4th convolution  layer -- ")
         x = self.dropout30(x)
-        x = self.conv5_bn(F.relu(self.conv5(x)))
-        # print(x.shape, " -- after 5th convolution  layer--")
-        x = self.pool(self.conv6_bn(F.relu(self.conv6(x))))
+        # x = self.conv5_bn(F.relu(self.conv5(x)))
+        # # print(x.shape, " -- after 5th convolution  layer--")
+        # x = self.pool(self.conv6_bn(F.relu(self.conv6(x))))
         # x = self.pool(self.conv7_bn(F.relu(self.conv7(x))))
-        
-        # print(x.shape, "-- after 6th convolution layer --")
-        x = self.dropout40(x)
-        # flatten output of third convolutional layer into a vector
-        # this vector is passed through the fully-connected nn
-        # x = x.view(-1, 128 * 28 * 28)
+
         x = x.view(x.size(0), -1)
-        # print(x.shape, "-- after the view call -- ")
-        # add dropout layer
-        # add 1st hidden layer, with relu activation function
         x = F.relu(self.fc1(x))
         # print(x.shape, "After 1st full connect layer")
-        # add dropout layer
-        x = self.dropout50(x)
-        # add 2nd hidden layer, without relu activation function
-        x = self.fc2(x)
-        # x = F.relu(self.fc2(x))
+        x = self.dropout30(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # x = self.sigmoid(self.fc3(x))
         return x
 
 
-model = ConvNet()
-model.to(mps_device)
+model = ConvNet().to(mps_device)
 
 # Hyper Parameters
-num_epochs = 5
+num_epochs = 10
 weight_decay = 5e-4
 learning_rate = 0.001
 # eof Hyper Parameters
@@ -234,14 +212,9 @@ def train(epoch):
 
         # Forward pass
         outputs = model(images)
-        # print("--- Shape of Image before hitting the loss function, ", outputs.size())
-        # y_output = outputs.round()
-        # print("--- Size of the tensor after the round -- ", y_output)
-        # torch.argmax something to look at using
-        outputs = torch.sigmoid(outputs)
+        # outputs = F.sigmoid(outputs)
 
         loss = criterion(outputs, labels)
-
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
@@ -273,43 +246,33 @@ for epoch in range(1, num_epochs + 1):
 
 def test():
     model.eval()
-    test_correct, test_total = 0.0, 0.0
-    y_test, y_pred = [], []
+    test_predictions = []
+    test_labels = []
 
     with torch.no_grad():
         for images, labels in test_loader:
             images = images.to(mps_device)
             labels = labels.to(mps_device)
             outputs = model(images)
-            outputs = F.sigmoid(outputs)
-            predicted = outputs.round()
-            # predicted = np.round(outputs)
-            # predicted = predicted.to(mps_device)
-            # _, predicted = torch.max(outputs.data, 1)
-            # _, predicted = torch.ma(outputs.data, 1)
-            # test_total += labels.size(0)
-            # test_correct += (predicted == labels).sum().item()
-            # test_correct += (predicted == labels.long()).sum().item()
-            y_test += labels.tolist()
-            y_pred += predicted.tolist()
+            predicted_probs = torch.sigmoid(outputs)
+            predicted_labels = torch.round(predicted_probs)
+            test_predictions.append(predicted_labels.cpu().numpy())
+            test_labels.append(labels.cpu().numpy())
 
-    macro_f1 = f1_score(y_test, y_pred, average='macro')
-    acc = accuracy_score(y_test, y_pred)
-    print('Test accuracy: %.4f, macro f1_score: %.4f' % (acc, macro_f1))
+    test_predictions = np.concatenate(test_predictions)
+    test_labels = np.concatenate(test_labels)
 
-    return y_test, y_pred
+    macro_f1 = f1_score(test_labels, test_predictions, average='macro')
+    accuracy = accuracy_score(test_labels, test_predictions)
+    print('Test accuracy: %.4f, macro f1_score: %.4f' % (accuracy, macro_f1))
+
+    return test_labels, test_predictions
 
 
 # Test the model
-y_test, y_pred = test()
+test_labels, test_predictions = test()
 
-confusion = multilabel_confusion_matrix(y_test, y_pred)
-# print('Confusion Matrix\n')
+# Confusion Matrix
+confusion = multilabel_confusion_matrix(test_labels, test_predictions)
+print('Confusion Matrix\n')
 print(confusion)
-
-acc = accuracy_score(y_test, y_pred)
-macrof1 = f1_score(y_test, y_pred, average='macro')
-microf1 = f1_score(y_test, y_pred, average='micro')
-print('Accuracy: {:.2f}'.format(acc))
-print('Macro F1-score: {:.2f}'.format(macrof1))
-print('Micro F1-score: {:.2f}'.format(microf1))
