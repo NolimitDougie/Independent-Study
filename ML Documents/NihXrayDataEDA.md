da45ce588616d7c9b851e0c6d11d1ab415748bf8
dougietownsell <townsell.4@wright.edu>
2023-06-21 23:34:26 -0400

    Updates to NIH code & NIH Xrag EDA markdown file

--- ML Documents/NihXrayDataEDA.md
+++ ML Documents/NihXrayDataEDA.md
@@ -1,9 +1,8 @@
 # NIH Chest Xray Dataset 
 
+Image Size 1024 * 1024 
 
-
-
-
+Data set includes patient information such as patient id, age, gender, and follow-up visits 
 
 ## Data Distribution 
 
@@ -26,22 +25,31 @@ Infiltration|Nodule           829
 ```
 
 
+### Data Graph
+
+![img.png](img.png)
+
 ## Implementation 
 
+NIH Chest X-ray Dataset consists of 112,120 X-ray images with disease labels from 30,805 unique patients. 
+I read in the data into all_xray_df dataframe for the labels on each image. I set up and auto encoder to represent the multiple disease in the images
 
+```
+learning_rate = 0.001
+criterion = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+```
 
-
-
+In my model, I employed the BCEWithLogitsLoss() loss function, which applies a sigmoid transformation to the data, resulting in values between 0 and 1. 
+To align these values with the label tensor, I utilized a threshold to round the data down to 0 or up to 1, effectively converting it into binary form. 
+By adjusting the model's threshold, I was able to observe changes in accuracy and the true positive rate within the multi-label confusion matrix for each class. 
 
 ## Model 
 
+Model - DenseNet 121 
+Model  - A custom architecture with 4 convolution filters and 3 fully connected layers 
 
-
-
-
-
-
-## Test Results 
+## Model Accuracy 
 
 
 
--- NihXrayNN.py
+++ NihXrayNN.py
@@ -13,24 +13,22 @@ import torchvision
 import seaborn as sns
 from tqdm import tqdm
 from PIL import Image
+from itertools import chain
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score, \
+    multilabel_confusion_matrix
 
 # Device configuration
 if torch.backends.mps.is_available():
     mps_device = torch.device("mps")
-    print("MPS device found")
 else:
     print("MPS device not found.")
 
-# Paths to files and DataEntry file
+# Paths to Images and DataEntry file
 all_xray_df = pd.read_csv('NihXrayData/Data_Entry_2017.csv')
 allImagesGlob = glob('NihXrayData/images*/images/*.png')
-bbox_list_df = pd.read_csv('NihXrayData/BBox_List_2017.csv')
-bbox_list_df.head(5)
 # eof
 
 all_image_paths = {os.path.basename(x): x for x in
@@ -45,9 +43,8 @@ all_xray_df.sample(3)
 # count_per_unique_label = all_xray_df['Finding Labels'].value_counts()[:15]  # get frequency counts per label
 # print(count_per_unique_label)
 # exit()
-
-# Data Pre Processing ####
-# define condition labels for one hot encoding - simplifying to 14 primary classes (excl. No Finding)
+# # Data Pre Processing ####
+# # Simplifying to 14 primary classes (excl. No Finding)
 condition_labels = ['Atelectasis', 'Consolidation', 'Infiltration', 'Pneumothorax', 'Edema', 'Emphysema', 'Fibrosis',
                     'Effusion', 'Pneumonia', 'Pleural_Thickening',
                     'Cardiomegaly', 'Nodule', 'Mass', 'Hernia']
@@ -60,7 +57,7 @@ all_xray_df['disease_vec'] = all_xray_df.apply(lambda target: [target[condition_
 all_xray_df.head()
 # eof of one hot encoding
 
-# Data Splitting ###
+# Splitting the Data Frames into 80:20 split ###
 train_df, test_df = train_test_split(all_xray_df, test_size=0.20, random_state=2020)
 #  eof Data Splitting ###
 
@@ -83,26 +80,26 @@ class XrayDataset(torch.utils.data.Dataset):
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
-        return transform(data), torch.Tensor(label)
+        return transform(data), torch.FloatTensor(label)
 
     def __len__(self):
         return len(self.data_frame)
 
 
-# Creating the Dataset and loader
+# Creating the Dataset for the train & test data frame
 test_dataset = XrayDataset(test_df)
 train_dataset = XrayDataset(train_df)
 
 test_loader = torch.utils.data.DataLoader(
     test_dataset,
-    batch_size=5000,
+    batch_size=1000,
     num_workers=0,
     shuffle=True,
 )
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=3000,
+    batch_size=5000,
     num_workers=0,
     shuffle=True,
 )
@@ -128,27 +125,18 @@ class ConvNet(nn.Module):
         super(ConvNet, self).__init__()
 
         # Image size 256 * 256 * 3 input channels
-        # 1. convolutional layer
         self.conv1 = nn.Conv2d(3, 32, 3)
         self.conv1_bn = nn.BatchNorm2d(32)
-        # 2. convolutional layer
         self.conv2 = nn.Conv2d(32, 32, 3)
         self.conv2_bn = nn.BatchNorm2d(32)
-        # 3. convolutional layer
         self.conv3 = nn.Conv2d(32, 64, 3)
         self.conv3_bn = nn.BatchNorm2d(64)
-        # 4 convolution Layer
         self.conv4 = nn.Conv2d(64, 128, 3)
         self.conv4_bn = nn.BatchNorm2d(128)
-        # output tensor 61 * 61 * 64
-        # 5. convolutional layer
         # self.conv5 = nn.Conv2d(64, 128, 3)
         # self.conv5_bn = nn.BatchNorm2d(128)
-        # # outputs 59 * 59 * 128 filter images
-        # # 6 convolutional layer
         # self.conv6 = nn.Conv2d(128, 128, 3)
         # self.conv6_bn = nn.BatchNorm2d(128)
-        # outputs 28 * 28 * 128 filtered Images
         # self.conv7 = nn.Conv2d(128, 128, 3)
         # self.conv7_bn = nn.BatchNorm2d(128)
 
@@ -156,13 +144,10 @@ class ConvNet(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
 
         # 1. fully-connected layer
-        # Input is a flattened 28*28*128 dimensional vector
         self.fc1 = nn.Linear(128 * 61 * 61, 128)
         self.fc1_bn = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 100)
         self.fc3 = nn.Linear(100, 14)
-        # self.output = nn.TransformerDecoder()
-        # self.sigmoid = nn.Sigmoid()
 
         # definition of dropout (dropout probability 25%)
         self.dropout20 = nn.Dropout(0.2)
@@ -176,25 +161,36 @@ class ConvNet(nn.Module):
         x = self.pool(self.conv4_bn(F.relu(self.conv4(x))))
         x = self.dropout30(x)
         # x = self.conv5_bn(F.relu(self.conv5(x)))
-        # # print(x.shape, " -- after 5th convolution  layer--")
         # x = self.pool(self.conv6_bn(F.relu(self.conv6(x))))
         # x = self.pool(self.conv7_bn(F.relu(self.conv7(x))))
 
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        # print(x.shape, "After 1st full connect layer")
         x = self.dropout30(x)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        # x = self.sigmoid(self.fc3(x))
         return x
 
 
 model = ConvNet().to(mps_device)
+# class DenseNet121(nn.Module):
+#     def __init__(self):
+#         super(DenseNet121, self).__init__()
+#         self.densenet121 = torchvision.models.densenet121(weight_decay)
+#         self.densenet121.classifier = nn.Sequential(
+#             nn.Linear(1024, 13)
+#         )
+#
+#     def forward(self, x):
+#         x = self.densenet121(x)
+#         return x
+#
+#
+# model = DenseNet121().to(mps_device)
 
 # Hyper Parameters
-num_epochs = 10
-weight_decay = 5e-4
+num_epochs = 2
+weight_decay = 1e-1
 learning_rate = 0.001
 # eof Hyper Parameters
 criterion = nn.BCEWithLogitsLoss()
@@ -205,23 +201,25 @@ def train(epoch):
     model.train()
     running_loss = 0.0
     train_total, train_correct = 0.0, 0.0
-    for i, (images, labels) in enumerate(train_loader):
 
+    for i, (images, labels) in enumerate(train_loader):
         images = images.to(mps_device)
         labels = labels.to(mps_device)
-
         # Forward pass
-        outputs = model(images)
-        # outputs = F.sigmoid(outputs)
-
-        loss = criterion(outputs, labels)
-        # Backward and optimize
         optimizer.zero_grad()
+        outputs = model(images)
+
+        # Loss Function
+        loss = criterion(outputs, labels)
+
+        # predicted_labels = (outputs > 0.5).float()
+
+        # Backward and optimize
+        # optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
-        # _, train_predicted = torch.max(outputs.data, 1)
         # _, train_predicted = torch.argmax(y_output)
 
         train_total += labels.size(0)
@@ -229,14 +227,12 @@ def train(epoch):
         # y_train += labels.tolist()
         # y_pred += train_predicted.tolist()
 
+        # train_correct += (predicted_labels == labels).sum().item()
+
         if i % 200 == 0:
             print('Epoch: {} [{}/{} ({:.0f}%)]\tloss: {:.6f}'.format(
                 epoch, i * len(images), len(train_loader.dataset),
                        100. * i / len(train_loader), loss.item()))
-
-    # macro_f1 = f1_score(y_train, y_pred, average='macro')
-    # print("epoch (%d): Train accuracy: %.4f, f1_score: %.4f, loss: %.3f" % (
-    #     epoch, train_correct / train_total, macro_f1, running_loss / train_total))
 
 
 # Train the model
@@ -255,13 +251,14 @@ def test():
             labels = labels.to(mps_device)
             outputs = model(images)
             predicted_probs = torch.sigmoid(outputs)
-            predicted_labels = torch.round(predicted_probs)
+            # predicted_labels = torch.round(predicted_probs)
+            predicted_labels = (predicted_probs > 0.60).float()
+
             test_predictions.append(predicted_labels.cpu().numpy())
             test_labels.append(labels.cpu().numpy())
 
     test_predictions = np.concatenate(test_predictions)
     test_labels = np.concatenate(test_labels)
-
     macro_f1 = f1_score(test_labels, test_predictions, average='macro')
     accuracy = accuracy_score(test_labels, test_predictions)
     print('Test accuracy: %.4f, macro f1_score: %.4f' % (accuracy, macro_f1))
@@ -274,5 +271,12 @@ test_labels, test_predictions = test()
 
 # Confusion Matrix
 confusion = multilabel_confusion_matrix(test_labels, test_predictions)
+roc_Score = roc_auc_score(test_labels, test_predictions)
 print('Confusion Matrix\n')
 print(confusion)
+print('Roc Score\n')
+print(roc_Score)
+
+
+
+
