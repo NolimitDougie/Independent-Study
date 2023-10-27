0099d08a5d87c52fef90ce177e37ad4edcf643b6
dougietownsell <townsell.4@wright.edu>
2023-10-26 22:22:36 -0400

    Updating code for model

--- chestXrays.py
+++ chestXrays.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 import torch.nn as nn
 import torch
 import torchvision
-from torchvision.models import ResNet50_Weights
+from torchvision.models import ResNet50_Weights, DenseNet121_Weights
 import seaborn as sns
 from tqdm import tqdm
 from PIL import Image
@@ -21,6 +21,8 @@ from sklearn.utils import resample
 import torch.optim.lr_scheduler as lr_scheduler
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score, \
     multilabel_confusion_matrix, roc_curve, auc, classification_report
+from collections import Counter
+from math import ceil
 
 # Device configuration GPU support for MAC
 if torch.backends.mps.is_available():
@@ -29,7 +31,6 @@ else:
     print("MPS device not found.")
 
 # Paths to Images and DataEntry file
-# all_xray_df = pd.read_csv('NihXrayData/Data_Entry_2017.csv')
 all_xray_df = pd.read_csv('NihXrayData/Data_Entry_2017_v2020.csv')
 allImagesGlob = glob('NihXrayData/images*/images/*.png')
 # eof
@@ -39,11 +40,6 @@ all_image_paths = {os.path.basename(x): x for x in
 # print('Scans found:', len(all_image_paths), ', Total Headers', all_xray_df.shape[0])
 all_xray_df['path'] = all_xray_df['Image Index'].map(all_image_paths.get)
 all_xray_df.sample(3)
-
-# # # Filter out rows with 'No Finding'
-# all_xray_df = all_xray_df[all_xray_df['Finding Labels'] != 'No Finding']
-# # Reset the index
-# all_xray_df.reset_index(drop=True, inplace=True)
 
 # # Data Pre Processing ####
 # # Simplifying to 15 primary classes (adding No Finding as the 15th class)
@@ -59,6 +55,9 @@ all_xray_df['disease_vec'] = all_xray_df.apply(lambda target: [target[condition_
 
 all_xray_df.head()
 
+print(all_xray_df[condition_labels].sum())
+exit()
+
 balanced_data = []
 # Target Samples for each class
 samples_per_class = 220
@@ -72,8 +71,6 @@ balanced_df = pd.concat(balanced_data)
 
 # Reset the index of the new DataFrame
 balanced_df.reset_index(drop=True, inplace=True)
-# all_xray_df = all_xray_df.sample(10000)
-
 
 # 70:30 split for Training and Testing data
 train_df, test_df = train_test_split(balanced_df, test_size=0.30, random_state=2020)
@@ -101,12 +98,11 @@ class XrayDataset(torch.utils.data.Dataset):
 
 # Define data augmentation for training
 train_transform = transforms.Compose([
-    # transforms.RandomResizedCrop(224),
     transforms.Resize(224),
     transforms.RandomHorizontalFlip(),
     transforms.RandomVerticalFlip(),
     transforms.RandomRotation(10),
-    transforms.RandomGrayscale(p=0.4),
+    transforms.RandomGrayscale(p=0.3),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
@@ -118,47 +114,73 @@ test_dataset = XrayDataset(test_df, transform=transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ]))
-
 # Data Loaders
 train_loader = DataLoader(
     train_dataset,
-    batch_size=64,
+    batch_size=16,
     num_workers=0,
     shuffle=True,
 )
 
 test_loader = torch.utils.data.DataLoader(
     test_dataset,
-    batch_size=64,
+    batch_size=16,
     num_workers=0,
     shuffle=False,
 )
 
 # # Load pre-trained ResNet50 model
 base_model = torchvision.models.resnet50(weights=ResNet50_Weights.DEFAULT)
+
 # Freeze the parameters of the base model
 for param in base_model.parameters():
     param.requires_grad = False
 
 # Replace the last fully connected layer with a new one for multi-label classification
 num_features = base_model.fc.in_features
-base_model.fc = nn.Linear(num_features, 15)
 
 
-# Create the final model
+# Create custom classifier head
+class CustomHead(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super(CustomHead, self).__init__()
+        self.block = nn.Sequential(
+            nn.Linear(in_features, hidden_features),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),  # Add Dropout layer with 30% drop rate
+            nn.Linear(hidden_features, out_features)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+# Attach the custom head to the base model
+base_model.fc = CustomHead(num_features, hidden_features=256, out_features=15)
+
+# # Create the final model
 model = base_model.to(mps_device)
+
+# # Freeze the parameters of the base model
+# for param in base_model.parameters():
+#     param.requires_grad = False
+#
+# # Replace the last fully connected layer with a new one for multi-label classification
+# num_features = base_model.fc.in_features
+# base_model.fc = nn.Linear(num_features, 15)
+#
+# # Create the final model
+# model = base_model.to(mps_device)
 # Print the model summary
 # print(model)
 
 # Hyperparameters/Loss Function
 num_epochs = 1
 weight_decay = 1e-4
-learning_rate = 0.001
-# learning_rate = 0.01
+learning_rate = 0.01
 
 criterion = nn.BCEWithLogitsLoss().to(mps_device)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), weight_decay=weight_decay)
 # eof Hyper Parameters/Loss Function
 scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
@@ -195,58 +217,145 @@ for epoch in range(1, num_epochs + 1):
     train(epoch)
 
 
+def feedback_on_predictions(y_true, y_pred, label_count):
+    if not (0 <= y_true.min() <= y_true.max() <= 1) or not (0 <= y_pred.min() <= y_pred.max() <= 1):
+        raise ValueError("Expected binary matrices for y_true and y_pred.")
+
+    # Count correct positive predictions per instance
+    correct_positive_count_per_instance = np.sum((y_true == 1) & (y_pred == 1), axis=1)
+
+    # Get the count of each type of correct positive predictions
+    count_statistics = Counter(correct_positive_count_per_instance)
+
+    # Display the feedback
+    x = list(range(label_count + 1))
+    y = []
+
+    for i in x:
+        count = count_statistics.get(i, 0)
+        percentage = (count / len(y_true)) * 100
+        y.append(percentage)
+        print(f"{count} X-ray images with exactly {i} correct positive predictions ({percentage:.2f}%).")
+
+    # Plotting
+    plt.plot(x, y, marker='o')
+    plt.xlabel('Number of True Positive Predictions')
+    plt.ylabel('Percentage of Total True Positive Predictions (%)')
+    plt.title('Evaluation of True Positive Prediction Correctness')
+    plt.xticks(x)
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
 def test(model, data_loader, device):
-    model.eval()
-    test_predictions = []
-    test_labels = []
-    class_accuracy = []
-    class_f1_score = []
+        model.eval()
+        test_predictions = []
+        test_labels = []
+        class_accuracy = []
+        class_f1_score = []
+        test_predictions_probs = []
 
-    with torch.no_grad():
-        for images, labels in data_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            predicted_probs = torch.sigmoid(outputs)
-            predicted_labels = (predicted_probs > 0.17).float()
+        with torch.no_grad():
+            for images, labels in data_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                predicted_probs = torch.sigmoid(outputs)
+                predicted_labels = (predicted_probs > 0.10).float()
 
-            test_predictions.append(predicted_labels.cpu().numpy())
-            test_labels.append(labels.cpu().numpy())
+                test_predictions.append(predicted_labels.cpu().numpy())
+                test_labels.append(labels.cpu().numpy())
+                test_predictions_probs.append(predicted_probs.cpu().numpy())
 
-    test_predictions = np.concatenate(test_predictions)
-    test_labels = np.concatenate(test_labels)
-    macro_f1 = f1_score(test_labels, test_predictions, average='weighted', zero_division=1)
-    # accuracy = accuracy_score(test_labels, test_predictions)
+        test_predictions = np.concatenate(test_predictions)
+        test_labels = np.concatenate(test_labels)
+        macro_f1 = f1_score(test_labels, test_predictions, average='macro', zero_division=1)
+        # accuracy = accuracy_score(test_labels, test_predictions)
 
-    # Calculate prediction accuracy, precision, and F1 score for each class
-    for i, class_label in enumerate(condition_labels):
-        class_accuracy.append(accuracy_score(test_labels[:, i], test_predictions[:, i]))
-        class_f1_score.append(f1_score(test_labels[:, i], test_predictions[:, i]))
+        # Calculate prediction accuracy, precision, and F1 score for each class
+        for i, class_label in enumerate(condition_labels):
+            class_accuracy.append(accuracy_score(test_labels[:, i], test_predictions[:, i]))
+            class_f1_score.append(f1_score(test_labels[:, i], test_predictions[:, i]))
 
-    print('Model Macro F1-score: %.4f' % macro_f1)
-    print('Model Accuracy: %.4f' % accuracy)
+        print('Model Macro F1-score: %.4f' % macro_f1)
+        print('Prediction Metrics per Class:')
+        for i, class_label in enumerate(condition_labels):
+            print('%s - Accuracy: %.4f, F1-score: %.4f' % (
+                class_label, class_accuracy[i], class_f1_score[i]))
 
-    print('Prediction Metrics per Class:')
-    for i, class_label in enumerate(condition_labels):
-        print('%s - Accuracy: %.4f, F1-score: %.4f' % (
-            class_label, class_accuracy[i], class_f1_score[i]))
+        # Compute the classification report
+        class_report = classification_report(test_labels, test_predictions, target_names=condition_labels, digits=2)
 
-    # Compute the classification report
-    class_report = classification_report(test_labels, test_predictions, target_names=condition_labels, digits=3)
+        print('Classification Report:')
+        print(class_report)
 
-    print('Classification Report:')
-    print(class_report)
+        feedback_on_predictions(test_labels, test_predictions, label_count=5)
 
-    return test_labels, test_predictions, class_accuracy, class_f1_score
+        return test_labels, test_predictions, class_accuracy, class_f1_score
 
 
 test_labels, test_predictions, class_accuracy, class_f1_score = test(model, test_loader, mps_device)
 
-# Multi-Label Confusion Matrix
-confusion = multilabel_confusion_matrix(test_labels, test_predictions)
-print('Confusion Matrix\n')
-for i, cm in enumerate(confusion):
-    print(f'Class {condition_labels[i]}:\n{cm}\n')
+
+def plot_multilabel_confusion_matrices(y_true, y_pred, class_names, cmap=plt.cm.Blues, save_hernia_as_png=False):
+    """
+    Plot confusion matrices for multilabel data using seaborn's heatmap.
+
+    Parameters:
+    - y_true: True labels
+    - y_pred: Predicted labels
+    - class_names: Names of the classes/labels
+    - cmap: Color map for the heatmap
+    - save_hernia_as_png: If True, save the confusion matrix for Hernia as a PNG
+    """
+    confusion = multilabel_confusion_matrix(y_true, y_pred)
+
+    # Calculate the number of rows and columns for the subplots
+    n_labels = len(class_names)
+    n_cols = 3  # you can adjust this number based on your preference
+    n_rows = ceil(n_labels / n_cols)
+
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, n_rows * 5))
+
+    # Make sure axes is always a 2D array, even when n_rows is 1
+    if n_rows == 1:
+        axes = np.reshape(axes, (1, -1))
+
+    for i, cm in enumerate(confusion):
+        row, col = divmod(i, n_cols)
+        ax = axes[row, col]
+
+        sns.heatmap(cm, annot=True, fmt='d', cmap=cmap, ax=ax,
+                    xticklabels=['False', 'True'],
+                    yticklabels=['False', 'True'])
+
+        ax.set_title(f"Confusion Matrix: {class_names[i]}")
+        ax.set_ylabel('True label')
+        ax.set_xlabel('Predicted label')
+
+        # Check if the current label is 'Hernia' and save it as PNG
+        if class_names[i] == "Hernia" and save_hernia_as_png:
+            temp_fig = plt.figure()
+            temp_ax = temp_fig.add_subplot(111)
+            sns.heatmap(cm, annot=True, fmt='d', cmap=cmap, ax=temp_ax,
+                        xticklabels=['False', 'True'],
+                        yticklabels=['False', 'True'])
+            temp_ax.set_title(f"Confusion Matrix: {class_names[i]}")
+            temp_ax.set_ylabel('True label')
+            temp_ax.set_xlabel('Predicted label')
+            temp_fig.savefig("hernia_confusion_matrix.png")
+            plt.close(temp_fig)
+
+    # Delete any unused subplots
+    for j in range(i + 1, n_rows * n_cols):
+        fig.delaxes(axes.flatten()[j])
+
+    plt.tight_layout()
+    plt.show()
+
+
+# Assuming test_labels and test_predictions are defined
+plot_multilabel_confusion_matrices(test_labels, test_predictions, condition_labels, save_hernia_as_png=True)
 
 # Create plot
 fig, c_ax = plt.subplots(1, 1, figsize=(9, 9))
